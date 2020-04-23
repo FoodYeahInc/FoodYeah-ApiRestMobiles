b4b6b7df8900881a038f65af40d7bd4f73aeff08
Akiraki <akiraki.awa@gmail.com>
2020-04-22 22:44:40 -0500

    eliminando una carpeta

--- Curso-DesarrolloWeb-SpringBoot-master/Curso-DesarrolloWeb-SpringBoot-master/0-ProyectoDemo/implementaciondto/src/main/java/com/example/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.model.Category;
-
-
-public interface CategoryService  {
-
-
-}
--- Curso-DesarrolloWeb-SpringBoot-master/Curso-DesarrolloWeb-SpringBoot-master/1-ProyectoCursoSpringBootAPIREST/src/main/java/com/example/repository/InvoiceItemsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.InvoiceItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceItemsRepository extends JpaRepository<InvoiceItem,Long> {
-}
